edd0a32994f1e33ed3d705fcf8fa78f454f32dcd
jiyoon0 <117243845+jiyoon0@users.noreply.github.com>
2022-11-02 11:53:04 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int main()
+void write_file() 
 {
-  printf("Hello");
+	FILE* fp = fopen("test.txt", "w");
+	int a = 1, b = 2, c = 3;
+	fprintf(fp, "%d %d %d", a, b, c);
+	fclose(fp);
+}
+
+void read_file() 
+{
+
+	FILE* fp = fopen("test.txt", "r");
+	int a, b, c;
+	fscanf(fp, "%d %d %d", &a, &b, &c);
+	printf("%d %d %d", a, b, c);
+	fclose(fp);
+}
+
+void append_file()
+{
+	int a = 4, b = 5, c = 6;
+	FILE* fp = fopen("test.txt", "a");
+	fprintf(fp, "%d %d %d", a, b, c);
+	fclose(fp);
+}
+
+int main() 
+{
+	write_file();
+	read_file();
+	append_file();
 }
